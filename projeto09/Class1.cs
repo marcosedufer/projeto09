c949e35da84636157a0b5ab1d8be496c036536cd
Windows 10 <Windows 10@WINDOWS-10>
2026-09-13 13:44:40 -0300

    Adicionar arquivos de projeto.

---
+++ projeto09/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto09
+{
+    class Retangulo
+    {
+        // Atributos privados
+        private int n1;
+        private int n2;
+        private int resultado;
+
+        // Set para o primeiro valor
+        public void setN1(int valor)
+        {
+            n1 = valor;
+        }
+
+        // Set para o segundo valor
+        public void setN2(int valor)
+        {
+            n2 = valor;
+        }
+
+        // Get para o primeiro valor
+        public int getN1()
+        {
+            return n1;
+        }
+
+        // Get para o segundo valor
+        public int getN2()
+        {
+            return n2;
+        }
+
+        // Get para o resultado
+        public int getResultado()
+        {
+            return resultado;
+        }
+
+        // Calcula a área
+        public void calcular()
+        {
+            // base * altura
+            resultado = n1 * n2;
+        }
+    }
+}
---
+++ projeto09/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace projeto09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // criação do objeto
+            Retangulo retangulo = new Retangulo();
+
+            // entrar com os valores de base e altura do retangulo
+            retangulo.setN1(entrarInt("Digite o valor de base do retangulo:\n- "));
+            retangulo.setN2(entrarInt("Digite o valor de altura do retangulo:\n- "));
+
+            // calcular a área do retangulo
+            retangulo.calcular();
+
+            // exibir o resultado da área do retangulo
+            Console.WriteLine("A área do retangulo é base {0} x altura {1} = {2}",
+                retangulo.getN1(),
+                retangulo.getN2(),
+                retangulo.getResultado());
+
+            // Verifica se o terreno é grande ou pequeno
+            if (retangulo.getResultado() > 100)
+            {
+                Console.WriteLine("Terreno grande");
+            }
+            else
+            {
+                Console.WriteLine("Terreno pequeno");
+            }
+        }
+        // Função para entrar com um valor inteiro
+        static int entrarInt(string mensagem)
+        {
+            Console.Write(mensagem);
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}        
